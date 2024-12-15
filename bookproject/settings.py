c9a3465ad3faf121a50d5f4de4a7788a7623302e
dheasalsabela <dhea.salsabela.tik22@mhsw.pnj.ac.id>
2024-12-15 18:51:01 +0700

    merevisi review

--- bookproject/settings.py
+++ bookproject/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # AUTH_USER_MODEL = 'books.Customuser'
 LOGIN_REDIRECT_URL = 'books:dashboard'
-LOGOUT_REDIRECT_URL = 'books:login'
+# LOGOUT_REDIRECT_URL = 'books:login'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
